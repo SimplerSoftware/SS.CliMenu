7d9cb8ed33ac9c885cfb312c13024f88a8fe3fab
John Carew <johnwcarew@gmail.com>
2019-07-22 16:10:37 -0500

    Update build script to set environment vars

--- build/Update-ModuleManifest.ps1
+++ build/Update-ModuleManifest.ps1
@@ -10,33 +10,36 @@ param(
 	[string] $ReleaseBranch = 'refs/heads/release'
 )
 $ModuleManifest = Get-Content $ManifestPath -Raw
-$rev = $null
-$sVersion
+$Env:Rev = $null
+$Env:Version = $null
 if ($ModuleManifest -match "(ModuleVersion\s*=)\s*'(.*)'"){
 	Write-Verbose "ModuleVersion matched [$($Matches[2])]"
 	if ($Matches[2] -match "(?<Major>0|(?:[1-9]\d*))(?:\.(?<Minor>0|(?:[1-9]\d*))(?:\.(?<Patch>0|(?:[1-9]\d*)))?(?:\-(?<PreRelease>[0-9A-Z\.-]+))?(?:\+(?<Meta>[0-9A-Z\.-]+))?)?"){
 		Write-Verbose "SymVer matched [$($Matches[0])]"
 		$aVersion = $Version -split "\." | select -Last 2
-		$rev = $aVersion[1]
-		$sVersion = "$($Matches.Major).$($Matches.Minor).$($aVersion[0])"
-		$PSModuleVersion = $sVersion
+		$Env:Rev = $aVersion[1]
+		$Env:Version = "$($Matches.Major).$($Matches.Minor).$($aVersion[0])"
+		$env:PSModuleVersion = $Env:Version
 	}
 }
 if (!$Prerelease -and $Branch -inotlike "$ReleaseBranch*"){
-	$Prerelease = "preview$rev"
-	$PSModuleVersion += "-$Prerelease"
+	$Env:Prerelease = "preview$Env:Rev"
+	$env:PSModuleVersion += "-$Env:Prerelease"
 }
 $date = Get-Date -Format "M/d/yyyy h:mm:ss tt"
-$ModuleManifest = $ModuleManifest -replace "(ModuleVersion\s*=)\s*'(.*)'", "`$1 '$sVersion'"
+$ModuleManifest = $ModuleManifest -replace "(ModuleVersion\s*=)\s*'(.*)'", "`$1 '$Env:Version'"
 $ModuleManifest = $ModuleManifest -replace "(Generated on:)\s*(.*\s(?:AM|PM))", "`$1 $date"
-if ($Prerelease){
-	Write-Host "Updating module manifest with pre-release version $sVersion-$Prerelease"
-	$ModuleManifest = $ModuleManifest -replace "(?:#\s*)?(Prerelease\s*=)\s*'(.*)'", "`$1 '$Prerelease'"
+if ($Env:Prerelease){
+	Write-Host "Updating module manifest with pre-release version $Env:Version-$Env:Prerelease"
+	$ModuleManifest = $ModuleManifest -replace "(?:#\s*)?(Prerelease\s*=)\s*'(.*)'", "`$1 '$Env:Prerelease'"
 } else {
-	Write-Host "Updating module manifest with release version $sVersion"
+	Write-Host "Updating module manifest with release version $Env:Version"
 	$ModuleManifest = $ModuleManifest -replace "(?:#\s*)?(Prerelease\s*=)\s*'(.*)'", "#`$1 '`$2'"
 }
 $ModuleManifest = $ModuleManifest -replace "[\s\t\r\n]*$", "" # Fix extra lines at EOF
 $ModuleManifest | Out-File -LiteralPath $ManifestPath
-#Write-Host "##vso[build.updatebuildnumber]$PSModuleVersion"
-Write-Host "##vso[task.setvariable variable=PSModuleVersion]$PSModuleVersion"
+#Write-Host "##vso[build.updatebuildnumber]$env:PSModuleVersion"
+Write-Host "##vso[task.setvariable variable=Version]$env:Version"
+Write-Host "##vso[task.setvariable variable=Rev]$env:Rev"
+Write-Host "##vso[task.setvariable variable=Prerelease]$env:Prerelease"
+Write-Host "##vso[task.setvariable variable=PSModuleVersion]$env:PSModuleVersion"
